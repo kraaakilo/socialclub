6d0ed178b0ec5aeca4e960fccf30d17ac76794a4
Kraaakilo <marcelvishnk@gmail.com>
2023-01-21 19:04:36 +0100

    Initialization

---
+++ src/main/java/dev/kraaakilo/socialclub/SocialclubApplication.java
@@ -0,0 +1,13 @@
+package dev.kraaakilo.socialclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialclubApplication.class, args);
+	}
+
+}
